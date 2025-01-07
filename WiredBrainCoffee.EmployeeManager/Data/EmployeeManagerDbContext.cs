d4c13e51fd34afb7f4fea39c612a7a7d0478b162
Rayhan Baloch <rehan3517@outlook.com>
2025-01-07 20:01:36 +0500

    Added Department Scaffolded CRUD pages and displayed Department of employees in list and employee detail

--- WiredBrainCoffee.EmployeeManager/Data/EmployeeManagerDbContext.cs
+++ WiredBrainCoffee.EmployeeManager/Data/EmployeeManagerDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using WiredBrainCoffee.EmployeeManager.Data;
 
 namespace WiredBrainCoffee.EmployeeManager.Data
 {
@@ -15,5 +16,17 @@ namespace WiredBrainCoffee.EmployeeManager.Data
         }
 
         public DbSet<Employee> Employee { get; set; } = default!;
+        public DbSet<Department> Department { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Department>().HasData(
+                new Department { Id = 1, Name = "IT"}
+                );
+
+            modelBuilder.Entity<Employee>().Property(e => e.DepartmentId)
+                .HasDefaultValue(1);
+
+        }
     }
 }
